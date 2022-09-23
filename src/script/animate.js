26a68836131cfee9714f5f038b8ecee0e21490bf
ColinSeger <colin.segerlund@elev.ga.ntig.se>
2022-09-23 11:45:51 +0200

    🚧

--- src/script/animate.js
+++ src/script/animate.js
@@ -1,10 +1,17 @@
 $( document.body ).ready(function() {
     $(".img-container").click(function(){
+        $(".img-click").css({
+            "opacity": "0",
+            "transform": "scale(0)",
+            "visibillity": "hidden",
+            "z-index": "-1"
+        });
         if($(this).children(".img-click").css("opacity") == "0"){
             $(this).children(".img-click").css({
                 "opacity": "1",
                 "transform": "scale(1)",
-                "visibillity": "visible"
+                "visibillity": "visible",
+                "z-index": "90"
             });
         }
         else{           
