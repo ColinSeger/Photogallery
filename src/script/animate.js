db4b2ab98478a283c1f1274306a76a9c98ccd50e
ColinSeger <colin.segerlund@elev.ga.ntig.se>
2022-09-23 09:37:48 +0200

    🚧

--- src/script/animate.js
+++ src/script/animate.js
@@ -1,11 +1,18 @@
 $( document.body ).ready(function() {
-   
     $(".img-container").click(function(){
-        if($(this).hasClass("img-click")){
-                $(this).removeClass("img-click");
-            }
-            else{           
-                $(this).addClass("img-click");
-            }
+        if($(this).children(".img-click").css("opacity") == "0"){
+            $(this).children(".img-click").css({
+                "opacity": "1",
+                "transform": "scale(1)",
+                "visibillity": "visible"
+            });
+        }
+        else{           
+            $(this).children(".img-click").css({
+                "opacity": "0",
+                "transform": "scale(0)",
+                "visibillity": "hidden"
+            });
+        }
     })
 });
